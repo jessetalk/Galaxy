f2d5c7f79e5e2d814b51c905ab31427e90e4c1db
CxistHans <CxistHans@hotmail.com>
2020-03-04 15:49:18 +0800

    fix appsettings and remote service

--- Galaxy.Order.Api/GalaxyOrderWebModule.cs
+++ Galaxy.Order.Api/GalaxyOrderWebModule.cs
@@ -1,21 +1,24 @@
-﻿using Galaxy.Product.Contracts;
+﻿using Galaxy.Order.Contracts;
+using Galaxy.Product.Contracts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Volo.Abp;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.Autofac;
+using Volo.Abp.Http.Client;
 using Volo.Abp.Modularity;
 
 namespace Galaxy.Order.Api
 {
     [DependsOn(typeof(AbpAspNetCoreMvcModule))]
     [DependsOn(typeof(AbpAutofacModule), typeof(GalaxyOrderModule))]
+    [DependsOn(typeof(AbpHttpClientModule))]
     public class GalaxyOrderWebModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
         {
-            //创建动态客户端代理
+            ////创建动态客户端代理
             context.Services.AddHttpClientProxies(
                 typeof(GalaxyProductContractModule).Assembly, remoteServiceConfigurationName: "ProductStore"
             );
